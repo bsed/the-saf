4461377c00f6d99230ccef4b3a79d870b5a2c2d1
fengzhizi715@gmail.com <fengzhizi715@gmail.com@c983da92-3397-3773-d8ba-40896e206ba4>
2012-06-25 06:09:48 +0000

    在StringHelper中增加求2个字符串的并集、交集、差集的方法

--- trunk/SAF/src/cn/salesuite/saf/utils/StringHelper.java
+++ trunk/SAF/src/cn/salesuite/saf/utils/StringHelper.java
@@ -6,7 +6,13 @@ package cn.salesuite.saf.utils;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.security.MessageDigest;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
+import java.util.Map.Entry;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -185,4 +191,89 @@ public class StringHelper {
 			return "";
 		return str;
 	}
+	
+	/**
+	 * 求两个字符串数组的并集，利用set的元素唯一性  
+	 * @param arr1
+	 * @param arr2
+	 * @return
+	 */
+    public static String[] union(String[] arr1, String[] arr2) {  
+        Set<String> set = new HashSet<String>();  
+        for (String str : arr1) {  
+            set.add(str);  
+        }  
+        for (String str : arr2) {  
+            set.add(str);  
+        }  
+        String[] result = {};  
+        return set.toArray(result);  
+    }
+    
+    /**
+     * 求两个数组的交集 
+     * @param arr1
+     * @param arr2
+     * @return
+     */
+    public static String[] intersect(String[] arr1, String[] arr2) {  
+        Map<String, Boolean> map = new HashMap<String, Boolean>();  
+        LinkedList<String> list = new LinkedList<String>();  
+        for (String str : arr1) {  
+            if (!map.containsKey(str)) {  
+                map.put(str, Boolean.FALSE);  
+            }  
+        }
+        
+        for (String str : arr2) {  
+            if (map.containsKey(str)) {  
+                map.put(str, Boolean.TRUE);  
+            }  
+        }  
+  
+        for (Entry<String, Boolean> e : map.entrySet()) {  
+            if (e.getValue().equals(Boolean.TRUE)) {  
+                list.add(e.getKey());  
+            }  
+        }  
+  
+        String[] result = {};  
+        return list.toArray(result);  
+    }
+    
+    /**
+     * 求两个数组的差集  
+     * @param arr1
+     * @param arr2
+     * @return
+     */
+    public static String[] minus(String[] arr1, String[] arr2) {  
+        LinkedList<String> list = new LinkedList<String>();  
+        LinkedList<String> history = new LinkedList<String>();  
+        String[] longerArr = arr1;  
+        String[] shorterArr = arr2;  
+        //找出较长的数组来减较短的数组  
+        if (arr1.length > arr2.length) {  
+            longerArr = arr2;  
+            shorterArr = arr1;  
+        }  
+        for (String str : longerArr) {  
+            if (!list.contains(str)) {  
+                list.add(str);  
+            }  
+        }  
+        for (String str : shorterArr) {  
+            if (list.contains(str)) {  
+                history.add(str);  
+                list.remove(str);  
+            } else {  
+                if (!history.contains(str)) {  
+                    list.add(str);  
+                }  
+            }  
+        }  
+  
+        String[] result = {};  
+        return list.toArray(result);  
+    }  
 }
