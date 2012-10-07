2e4516609b24c970d4835912953c3ba49ac122a8
fengzhizi715@gmail.com <fengzhizi715@gmail.com@c983da92-3397-3773-d8ba-40896e206ba4>
2012-10-07 14:54:24 +0000

    修复https不能访问的bug

--- trunk/SAF/src/cn/salesuite/saf/http/MySSLSocketFactory.java
+++ trunk/SAF/src/cn/salesuite/saf/http/MySSLSocketFactory.java
@@ -6,59 +6,61 @@ package cn.salesuite.saf.http;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.security.KeyManagementException;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.UnrecoverableKeyException;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
-
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-
-import org.apache.http.conn.ssl.SSLSocketFactory;
+import java.security.*;
+import java.security.cert.*;
+import javax.net.ssl.*;
 
 /**
  * @author Tony Shen
  * 
  */
-public class MySSLSocketFactory extends SSLSocketFactory {
+public class MySSLSocketFactory extends
+		org.apache.http.conn.ssl.SSLSocketFactory {
 
-	SSLContext sslContext = SSLContext.getInstance("TLS");
+	private SSLSocketFactory sslFactory = HttpsURLConnection
+			.getDefaultSSLSocketFactory();
 
 	public MySSLSocketFactory(KeyStore truststore)
 			throws NoSuchAlgorithmException, KeyManagementException,
 			KeyStoreException, UnrecoverableKeyException {
-		super(truststore);
+		super(null);
 
-		TrustManager tm = new X509TrustManager() {
-			public void checkClientTrusted(X509Certificate[] chain,
-					String authType) throws CertificateException {
-			}
+		try {
+			SSLContext context = SSLContext.getInstance("TLS");
 
-			public void checkServerTrusted(X509Certificate[] chain,
-					String authType) throws CertificateException {
-			}
+			// Create a trust manager that does not validate certificate chains
+			// and simply
+			// accept all type of certificates
+			TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+				public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+					return new java.security.cert.X509Certificate[] {};
+				}
 
-			public X509Certificate[] getAcceptedIssuers() {
-				return null;
-			}
-		};
+				public void checkClientTrusted(X509Certificate[] chain,
+						String authType) throws CertificateException {
+				}
 
-		sslContext.init(null, new TrustManager[] { tm }, null);
+				public void checkServerTrusted(X509Certificate[] chain,
+						String authType) throws CertificateException {
+				}
+			} };
+
+			// Initialize the socket factory
+			context.init(null, trustAllCerts, new SecureRandom());
+			sslFactory = context.getSocketFactory();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public Socket createSocket(Socket socket, String host, int port,
 			boolean autoClose) throws IOException, UnknownHostException {
-		return sslContext.getSocketFactory().createSocket(socket, host, port,
-				autoClose);
+		return sslFactory.createSocket(socket, host, port, autoClose);
 	}
 
 	@Override
 	public Socket createSocket() throws IOException {
-		return sslContext.getSocketFactory().createSocket();
+		return sslFactory.createSocket();
 	}
 }
