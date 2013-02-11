e98a5b512e1c39ecc717a10caeeb3ee639db486c
fengzhizi715 <fengzhizi715@gmail.com>
2013-02-11 07:43:44 +0000

    SAF注解框架的代码优化

--- SAF/src/cn/salesuite/saf/inject/InjectException.java
+++ SAF/src/cn/salesuite/saf/inject/InjectException.java
@@ -4,6 +4,7 @@
 package cn.salesuite.saf.inject;
 
 /**
+ * 注解的异常类
  * @author Tony Shen
  *
  */
@@ -11,15 +12,15 @@ public class InjectException extends RuntimeException{
 
 	private static final long serialVersionUID = -5298989560573894243L;
 
-    public InjectException(String detailMessage) {
-        super(detailMessage);
+    public InjectException(String message) {
+        super(message);
     }
 
     public InjectException(Throwable throwable) {
         super(throwable);
     }
 
-    public InjectException(String detailMessage, Throwable throwable) {
-        super(detailMessage, throwable);
+    public InjectException(String message, Throwable throwable) {
+        super(message, throwable);
     }
 }
