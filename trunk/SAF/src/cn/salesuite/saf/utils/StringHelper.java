16cd961784a6188122ed7a4720f5024fdf6ed402
fengzhizi715@gmail.com <fengzhizi715@gmail.com@c983da92-3397-3773-d8ba-40896e206ba4>
2013-03-18 03:01:33 +0000

    修复验证email的合法性方法的bug

--- trunk/SAF/src/cn/salesuite/saf/utils/StringHelper.java
+++ trunk/SAF/src/cn/salesuite/saf/utils/StringHelper.java
@@ -45,7 +45,7 @@ public class StringHelper {
 	 * @return
 	 */
 	public static boolean checkEmail(String emailStr) {
-		String check = "^([a-z0-9A-Z]+[-|._]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?.)+[a-zA-Z]{2,}$";
+		String check = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
 		Pattern regex = Pattern.compile(check);
 		Matcher matcher = regex.matcher(emailStr.trim());
 		boolean isMatched = matcher.matches();
