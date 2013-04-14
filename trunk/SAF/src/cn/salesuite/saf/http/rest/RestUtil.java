88f8b785baed62ef6a49039ef4a39adc5a65784e
fengzhizi715@gmail.com <fengzhizi715@gmail.com@c983da92-3397-3773-d8ba-40896e206ba4>
2013-04-14 07:17:02 +0000

    提交http rest client的一些辅助类

---
+++ trunk/SAF/src/cn/salesuite/saf/http/rest/RestUtil.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package cn.salesuite.saf.http.rest;
+
+import java.io.IOException;
+
+import com.alibaba.fastjson.JSON;
+
+/**
+ * @author Tony
+ *
+ */
+public class RestUtil {
+	
+	/**
+	 * 验证charset
+	 * @param charset
+	 * @return
+	 */
+	public static String getValidCharset(String charset) {
+		if (charset != null && charset.length() > 0)
+			return charset;
+		else
+			return RestConstant.CHARSET_UTF8;
+	}
+	
+	/**
+	 * 根据返回的http body内容转换成json对象
+	 * @param cls
+	 * @param body
+	 * @return
+	 * @throws IOException
+	 */
+    public <T> T parseAs(Class<T> cls,String body) throws IOException {
+    	return JSON.parseObject(body, cls);
+    }
+}
