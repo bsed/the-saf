cc6be29be5a70be39e0c344cc666f9bd14ce9c0f
fengzhizi715@gmail.com <fengzhizi715@gmail.com@c983da92-3397-3773-d8ba-40896e206ba4>
2013-05-20 05:25:58 +0000

    增加eventbus框架的雏形

---
+++ trunk/SAF/src/cn/salesuite/saf/eventbus/BackgroundPoster.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package cn.salesuite.saf.eventbus;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
+
+
+/**
+ * 后台线程运行，使用CompletionService，从而无需使用阻塞队列
+ * @author Tony
+ *
+ */
+public class BackgroundPoster {
+	
+	private CompletionService<Void> service;
+	private final EventBus eventBus;
+
+	BackgroundPoster(EventBus eventBus) {
+		this.eventBus = eventBus;
+		service = new ExecutorCompletionService<Void>(
+				EventBus.executorService);
+	}
+
+	public void enqueue(final Object event,final EventHandler subscription) {
+		service.submit(new Callable<Void>(){
+			public Void call() throws Exception {
+				eventBus.invokeSubscriber(event,subscription);
+				return null;
+            }    
+		});
+		
+		try {
+			service.take().get();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
