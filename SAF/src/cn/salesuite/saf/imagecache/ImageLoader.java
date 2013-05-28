35d7fc2b61c4d574b37f81cc5194f586aa3319f1
fengzhizi715 <fengzhizi715@gmail.com>
2013-05-28 06:57:45 +0000

    ImageLoader增加displayImage() 用于可显示带圆角的图片

--- SAF/src/cn/salesuite/saf/imagecache/ImageLoader.java
+++ SAF/src/cn/salesuite/saf/imagecache/ImageLoader.java
@@ -14,6 +14,7 @@ import android.graphics.Bitmap;
 import android.os.Handler;
 import android.util.Log;
 import android.widget.ImageView;
+import cn.salesuite.saf.utils.BitmapHelper;
 
 /**
  * @author Tony Shen
@@ -66,7 +67,7 @@ public class ImageLoader {
      * 显示图片，可自定义默认显示的图片
      * @param url
      * @param imageView
-     * @param imageId 默认图片，可能区别与default_img_id
+     * @param imageId 默认图片，可能区别与default_img_id，一个app只有一个default_img_id，imageId可有很多个
      */
     public void displayImage(String url, ImageView imageView, int imageId) {
         imageViews.put(imageView, url);
@@ -75,6 +76,24 @@ public class ImageLoader {
             imageView.setImageBitmap(bitmap);
         else {
             queuePhoto(url, imageView, imageId);
+            imageView.setImageResource(imageId);
+        }
+    }
+
+    /**
+     * 显示图片，可自定义默认显示的图片
+     * @param url
+     * @param imageView
+     * @param imageId
+     * @param options 图片带JobOptions选项，可变成画圆角图形
+     */
+    public void displayImage(String url, ImageView imageView, int imageId,final JobOptions options) {
+        imageViews.put(imageView, url);
+        Bitmap bitmap=memoryCache.get(url);
+        if(bitmap!=null) {
+        	imageView.setImageBitmap(bitmap);
+        } else {
+            queuePhoto(url, imageView, imageId,options);
             imageView.setImageResource(imageId);
         }
     }
@@ -88,6 +107,12 @@ public class ImageLoader {
         PhotoToLoad p=new PhotoToLoad(url, imageView, imageId);
         executorService.submit(new PhotosLoader(p));
     }
+    
+    private void queuePhoto(String url, ImageView imageView, int imageId,
+			JobOptions options) {
+        PhotoToLoad p=new PhotoToLoad(url, imageView, imageId, options);
+        executorService.submit(new PhotosLoader(p));
+	}
     
     public Bitmap getBitmap(String url,ImageView imageView) {
         //from SD cache
@@ -142,6 +167,7 @@ public class ImageLoader {
         public String url;
         public ImageView imageView;
         public int imageId;
+        public JobOptions options;
         
         public PhotoToLoad(String u, ImageView i){
             url=u; 
@@ -153,6 +179,13 @@ public class ImageLoader {
             url=u; 
             imageView=i;
             this.imageId = imageId;
+        }
+        
+        public PhotoToLoad(String u, ImageView i, int imageId, JobOptions options){
+            url=u; 
+            imageView=i;
+            this.imageId = imageId;
+            this.options = options;
         }
     }
     
@@ -167,7 +200,11 @@ public class ImageLoader {
             if(imageViewReused(photoToLoad))
                 return;
             Bitmap bmp=getBitmap(photoToLoad.url,photoToLoad.imageView);
-            memoryCache.put(photoToLoad.url, bmp);
+            if (photoToLoad.options!=null) {
+            	memoryCache.put(photoToLoad.url, BitmapHelper.roundCorners(bmp , photoToLoad.options.cornerRadius));
+            } else {
+            	memoryCache.put(photoToLoad.url, bmp);
+            }
             if(imageViewReused(photoToLoad))
                 return;
             BitmapDisplayer bd=new BitmapDisplayer(bmp, photoToLoad);
@@ -195,8 +232,13 @@ public class ImageLoader {
         public void run(){
             if(imageViewReused(photoToLoad))
                 return;
-            if(bitmap!=null)
-                photoToLoad.imageView.setImageBitmap(bitmap);
+            if(bitmap!=null) {
+            	if (photoToLoad.options!=null) {
+            		photoToLoad.imageView.setImageBitmap(BitmapHelper.roundCorners(bitmap , photoToLoad.options.cornerRadius));
+            	} else {
+            		photoToLoad.imageView.setImageBitmap(bitmap);
+            	}
+            } 
             else
                 photoToLoad.imageView.setImageResource(photoToLoad.imageId);
         }
@@ -204,8 +246,6 @@ public class ImageLoader {
     
     public static class JobOptions {
 
-        public boolean roundedCorners = false;
-        public boolean fadeIn = true;
         public int cornerRadius = 5;
         public int requestedWidth;
         public int requestedHeight;
