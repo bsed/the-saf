0a4b463c7ab0de66549fc6b3a939903a75c8026d
fengzhizi715 <fengzhizi715@gmail.com>
2013-10-23 03:50:48 +0000

    BitmapConnection使用restclient

--- SAF/src/cn/salesuite/saf/imagecache/BitmapConnection.java
+++ SAF/src/cn/salesuite/saf/imagecache/BitmapConnection.java
@@ -5,10 +5,8 @@ package cn.salesuite.saf.imagecache;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
-import android.util.Log;
+import cn.salesuite.saf.http.rest.RestClient;
 
 /**
  * @author Tony Shen
@@ -19,28 +17,19 @@ class BitmapConnection {
 		T run(InputStream stream);
 	}
 
-	public <T> T readStream(final String urlString, final Runnable<T> runnable) {
-		HttpURLConnection urlConnection = null;
-		InputStream inputStream = null;
+	public <T> T readStream(String urlString,Runnable<T> runnable) {		
+		RestClient client = RestClient.get(urlString).useCaches(true);
+		InputStream inputStream = new PatchInputStream(client.stream());
 		try {
-			final URL url = new URL(urlString);
-			urlConnection = (HttpURLConnection) url.openConnection();
-			urlConnection.setUseCaches(true);
-			inputStream = new PatchInputStream(urlConnection.getInputStream());
 			return runnable.run(inputStream);
-		} catch (final IOException e) {
-			Log.e("BitmapConnection", "", e);
-			return null;
 		} finally {
 			if (inputStream != null) {
 				try {
 					inputStream.close();
-				} catch (final IOException e) {
+				} catch (IOException e) {
 				}
 			}
-			if (urlConnection != null) {
-				urlConnection.disconnect();
-			}
 		}
+
 	}
 }
